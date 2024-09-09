419a1f06e5ed90777ad3464bbdaad035f323c62e
unknown <mcclaude7>
2024-09-09 12:59:30 +0200

    updated api/views file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django_filters import rest_framework
 from .models import Book
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
