00c5d75395a640c3a66d81d555b112ab1b124d38
unknown <mcclaude7>
2024-09-09 12:18:34 +0200

    modified api/views.py file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Book
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly
 from .serializers import BookSerializer
 
 class ListView(generics.ListAPIView):
